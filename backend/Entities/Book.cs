f83e424ffbd20e735c7258fadb91e8ea612a47b8
kenshi <samuduchanuka@gmail.com>
2025-08-20 22:33:47 +0530

    Update book entitiy. Separate genre foreign key and navigation property. Add defualt of not read value to read status and make rating nullable

--- backend/Entities/Book.cs
+++ backend/Entities/Book.cs
@@ -5,12 +5,13 @@ public class Book
     public int BookId { get; set; }
     public required string Name { get; set; }
     public required int OwnerId { get; set; }
-    public BookGenre? Genre { get; set; }
+    public required int GenreId { get; set; }
     public required string Author { get; set; }
     public required int TotalPages { get; set; }
-    public ReadStatus Status { get; set; }
-    public int Rating { get; set; }
+    public ReadStatus Status { get; set; } = ReadStatus.Unread;
+    public int? Rating { get; set; }
     public string? Remarks { get; set; }
 
-    public required User Owner { get; set; }
+    public User? Owner { get; set; }
+    public BookGenre? Genre { get; set; }
 }
