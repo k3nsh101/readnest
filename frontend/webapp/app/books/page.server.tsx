110c369004750b7320267644163dda2a2f1a2be0
kenshi <samuduchanuka@gmail.com>
2025-08-22 15:34:29 +0530

    Fix import issue

--- frontend/webapp/app/books/page.server.tsx
+++ frontend/webapp/app/books/page.server.tsx
@@ -1,3 +1,5 @@
+import { Book } from "./interfaces";
+
 export async function fetchBooks(): Promise<Book[]> {
   const res = await fetch(`${process.env.BACKEND_API_BASE_URL}/books`);
   if (!res.ok) throw new Error("Failed to fetch books");
