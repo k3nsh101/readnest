fbc169102864f431ebb3a2c7aaec7ebd985b7126
kenshi <samuduchanuka@gmail.com>
2025-08-24 12:55:39 +0530

    Rename file name and add required property modifier

--- backend/Entities/BorrowedInfo.cs
+++ backend/Entities/BorrowedInfo.cs
@@ -2,7 +2,7 @@ namespace ReadNest.Entities;
 
 public class BorrowedInfo
 {
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
     public required Guid BookId { get; set; }
     public required string LenderName { get; set; }
     public required DateOnly BorrowedDate { get; set; }
