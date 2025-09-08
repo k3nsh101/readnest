f4c9388a100809b8f32e8a78ba5d99b901650bfd
kenshi <samuduchanuka@gmail.com>
2025-09-08 22:37:30 +0530

    Make the book list repsponsive

--- frontend/webapp/app/books/bookList.tsx
+++ frontend/webapp/app/books/bookList.tsx
@@ -7,20 +7,27 @@ export default function BooksList({ books }: BooksListProps) {
     return <p className="text-center mt-10">No books added yet.</p>;
 
   return (
-    <div className="flex gap-10 flex-wrap">
+    <div className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(180px,1fr))]">
       {books.map((book) => (
-        <div key={book.bookId} className="bg-transparent flex flex-col gap-1">
-          <Link href={`books/${book.bookId}`} className="h-full">
+        <div
+          key={book.bookId}
+          className="flex flex-col items-center max-w-[220px] mx-auto"
+        >
+          <Link href={`books/${book.bookId}`} className="w-full">
             {book.coverUrl && (
-              <Image
-                src={`${process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}${book.coverUrl}`}
-                alt={book.title}
-                width={170}
-                height={300}
-                className="rounded mb-2 h-full object-fill"
-              />
+              <div className="w-full h-[300px] flex items-center justify-center bg-gray-100 rounded mb-2">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}${book.coverUrl}`}
+                  alt={book.title}
+                  width={200}
+                  height={300}
+                  className="max-h-full object-contain"
+                />
+              </div>
             )}
-            <p className="font-bold">{book.title}</p>
+            <p className="font-bold text-center break-words line-clamp-2">
+              {book.title}
+            </p>
           </Link>
         </div>
       ))}
